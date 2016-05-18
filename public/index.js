8a229af1d9ef8f010b53e63c2fc9d52ed1dc6804
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-05-18 10:12:00 -0700

    add a counter

--- public/index.js
+++ public/index.js
@@ -2,15 +2,26 @@
 var Root = React.createClass({
   getInitialState: function(){
     return {
-      counter: 0
+      counters: {
+        one: 0,
+        two: 0
+      }
     }
   },
 
-  addCount: function(){
-    this.setState({counter: this.state.counter + 1})
+  addCount: function(event){
+    console.log('event', event.target)
+    var id = event.target.id
+    var newState = Object.create(this.state);
+    newState.counters[id]++;
+    this.setState(newState);
   },
-  minusCount: function(){
-    this.setState({counter: this.state.counter - 1})
+  minusCount: function(arg, event){
+    console.log('arg', arg, 'event', event.target)
+    var key = arg;
+    var newState = Object.create(this.state);
+    newState.counters[key]--;
+    this.setState(newState)
   },
 
   render: function() {
@@ -18,10 +29,17 @@ var Root = React.createClass({
       <div>
         <h1>Welcome To Managed Properties</h1>
         <p>Add a new property</p>
+
+        <h3>Counter 1</h3>
+        <button id="one" onClick={this.addCount}>+</button>
+        <button onClick={this.minusCount.bind(this, 'one')}>-</button>
+
+        <h3>Counter 2</h3>
         <button onClick={this.addCount}>+</button>
         <button onClick={this.minusCount}>-</button>
 
-        <h3>Button clicked: {this.state.counter}</h3>
+        <h3>Counter 1: {this.state.counters.one}</h3>
+        <h3>Counter 2: {this.state.counters.two}</h3>
 
       </div>
     )
