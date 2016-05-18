ced1ad98e18e1a0907f5f824ead89f88f7b870fc
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-05-18 11:47:49 -0700

    counter component with state

--- public/index.js
+++ public/index.js
@@ -1,4 +1,33 @@
 
+var Welcome = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Welcome To Managed Properties</h1>
+        <p>Add a new property</p>
+      </div>
+    )
+  }
+})
+
+var Counter = React.createClass({
+  getInitialState: function() {
+    return {
+      counter: 0
+    }
+  },
+
+  render: function() {
+    return (
+      <div>
+        <h3>Counter 2: {this.state.counter}</h3>
+        <button onClick={() => this.setState({counter: this.state.counter + 1})}>+</button>
+        <button onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>
+      </div>
+    )
+  }
+})
+
 var Root = React.createClass({
   getInitialState: function(){
     return {
@@ -27,19 +56,15 @@ var Root = React.createClass({
   render: function() {
     return (
       <div>
-        <h1>Welcome To Managed Properties</h1>
-        <p>Add a new property</p>
 
-        <h3>Counter 1</h3>
+        <Welcome />
+
+        <h3>Counter 1: {this.state.counters.one}</h3>
         <button id="one" onClick={this.addCount}>+</button>
         <button onClick={this.minusCount.bind(this, 'one')}>-</button>
 
-        <h3>Counter 2</h3>
-        <button onClick={this.addCount}>+</button>
-        <button onClick={this.minusCount}>-</button>
+        <Counter />
 
-        <h3>Counter 1: {this.state.counters.one}</h3>
-        <h3>Counter 2: {this.state.counters.two}</h3>
 
       </div>
     )
