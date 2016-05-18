2e6a721022ceef6466a9957710d1dd5bc2f230b3
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-05-18 09:41:27 -0700

    add/minus counter

--- public/index.js
+++ public/index.js
@@ -1,14 +1,34 @@
 
+var Root = React.createClass({
+  getInitialState: function(){
+    return {
+      counter: 0
+    }
+  },
 
-var Hello = React.createClass({
-  render: function(){
+  addCount: function(){
+    this.setState({counter: this.state.counter + 1})
+  },
+  minusCount: function(){
+    this.setState({counter: this.state.counter - 1})
+  },
+
+  render: function() {
     return (
-      <h1>Yo</h1>
+      <div>
+        <h1>Welcome To Managed Properties</h1>
+        <p>Add a new property</p>
+        <button onClick={this.addCount}>+</button>
+        <button onClick={this.minusCount}>-</button>
+
+        <h3>Button clicked: {this.state.counter}</h3>
+
+      </div>
     )
   }
 })
 
 ReactDOM.render(
-  <Hello />,
+  <Root />,
   document.getElementById('root')
 );
